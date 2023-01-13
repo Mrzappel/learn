65c032d8f2bae48eb8b571414ccd487ac55e0c36
Mrzappel <857463451@qq.com>
2023-01-13 17:24:02 +0800

    feat: ts

--- TS/hello.js
+++ TS/hello.js
@@ -6,3 +6,16 @@ var c = false;
 console.log(c);
 var d = [1, 2, 3];
 console.log(d);
+// 泛型
+function returnSelf(arg) {
+    return arg;
+}
+function returnLength(arg) {
+    console.log(arg.length);
+    return arg;
+}
+var s = returnSelf('ss');
+var v = returnSelf(111);
+var x = returnSelf(true);
+console.log(s, v, x);
+console.log(returnLength([12, 3, 4]));
--- TS/hello.ts
+++ TS/hello.ts
@@ -6,3 +6,19 @@ let c: boolean = false
 console.log(c);
 let d: number[] = [1, 2, 3]
 console.log(d)
+
+// 泛型
+function returnSelf<T>(arg: T): T {
+  return arg
+}
+
+// 泛型当作类型的一部分
+function returnLength<T>(arg: T[]): T[] {
+  console.log(arg.length)
+  return arg
+}
+const s = returnSelf('ss')
+const v = returnSelf(111)
+const x = returnSelf<boolean>(true)
+console.log(s, v, x)
+console.log(returnLength([12, 3, 4]))
