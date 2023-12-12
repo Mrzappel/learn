ce8caf68d560374a1d267f52fec61c27f132e1ce
mrzappel <78204527+Mrzappel@users.noreply.github.com>
2023-12-12 09:16:09 +0000

    refactor: Refactor arrayToTree function and add debounce function

--- interview/handle/arrayToTree.js
+++ interview/handle/arrayToTree.js
@@ -1,19 +1,5 @@
 // 数组转树，O（n）复杂度，测试数据
 
-// export const arrayToTree = (array) => {
-//   const tree = []
-//   const map = {}
-//   array.forEach(v => {
-//     v.children = []
-//     const { id, parentId, ...res } = v
-//     if (map[parentId]) {
-//       map[parentId].children = v || []
-//     } else {
-//       map[id] = v
-//     }
-//   })
-//   return tree
-// }
 const array = [
   { id: 1, parentId: null, name: 'Node 1' },
   { id: 2, parentId: 1, name: 'Node 2' },
@@ -22,60 +8,40 @@ const array = [
   { id: 5, parentId: 2, name: 'Node 5' },
   { id: 6, parentId: 3, name: 'Node 6' },
 ]
-// const arrayToTree = array => {
-//   const tree = []
+
+// const handleArray = array => {
 //   const map = {}
-//   array.forEach(v => {
-//     v.children = []
-//     const { id, parentId, ...res } = v
-//     map[id] = v
-//     if (map[parentId]) {
-//       map[parentId].children.push(v)
-//     } else if (parentId === null || parentId === undefined) {
-//       tree.push(v)
+//   const tree = []
+//   array.forEach(item => (map[item.id] = item))
+//   array.forEach(node => {
+//     const parent = map[node.parentId]
+//     if (parent) {
+//       ;(parent.children || (parent.children = [])).push(node)
+//     } else {
+//       tree.push(node)
 //     }
 //   })
 //   return tree
 // }
-// console.log(arrayToTree(array))
+const arrayToTree = (
+  array,
+  id = 'id',
+  parentId = 'parentId',
+  children = 'children'
+) => {
+  const map = {}
+  const tree = []
+  array.forEach(item => (map[item[id]] = item))
+  array.forEach(node => {
+    const parent = map[node[parentId]]
+    if (parent) {
+      ;(parent[children] || (parent[children] = [])).push(node)
+    } else {
+      tree.push(node)
+    }
+  })
+  return tree
+}
 
-// const handleArray = array => {
-//   const result = []
-//   const map = {}
-//   array.forEach(v => {
-//     const { id, parentId } = v
-//     v.children = []
-//     map[id] = v
-//     if (map[parentId]) {
-//       map[parentId].children.push(v)
-//     } else {
-//       result.push(v)
-//     }
-//   })
-//   return result
-// }
-
-// const handleArray = array => {
-//   let map = {}
-//   const result = []
-//   // map = array.reduce((acc,cur) => acc[cur.id] = cur,{})
-//   array.forEach(item => {
-//     item.children = []
-//     const {id} = item
-//     map[id] = item
-//   })
-//   array.forEach(v => {
-//     const { id, parentId } = v
-//     // v.children = []
-//     // map[id] = v
-//     const parent = map[parentId]
-//     if (parent) {
-//       parent.children.push(v)
-//     } else if(parentId === null){
-//       result.push(v)
-//     }
-//   })
-//   return result
-// }
-
-console.log(handleArray(array))
+console.log(arrayToTree(array))
+export { arrayToTree }
